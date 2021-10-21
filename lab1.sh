27215a4e4c8aa3a2b88e6c94810b4e18cf701f72
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-21 23:05:00 +0500

    Day1 Lab with echo

---
+++ lab1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo hi gayatri
+
+#escape sequences
+
+echo -e "line1\n line2"
+
+#colour codes
+
+echo -e "\e[31m Red Color code is 31"
+echo -e "\e[32m Green colour code is 32"
+echo -e "\e[33m Yellow color code is 33\e[0m"
+echo 34 is b for blue 35 is meganta and 36 is for cyan
