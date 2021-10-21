3d2bf819b520ce5fa38860ff6126fdf10cfc9a3a
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-21 23:12:24 +0500

    Created  a simple function

---
+++ functions.sh
@@ -0,0 +1,22 @@
+function abc() {
+  echo I am a function abc
+#  a=100
+ # echo a in function = $a
+ # b=20
+  #return 20
+  #echo First Argument in Function = $1
+}
+
+#xyz() {
+ # echo I am a function xyz
+#}
+
+##  Main Program
+a=10
+#abc Rahul
+#abc $1
+#echo Exit status of abc - $?
+#echo b in main program = $b
+#xyz
+
+#echo Frist Argument in Main Program = $1
