b39e554d5231703c539a3b91543f8ed54c319711
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-23 19:44:00 +0500

    modified script

--- functions.sh
+++ functions.sh
@@ -2,13 +2,7 @@ function abc() {
   echo I am a function abc
  a=100
   echo a in function = $a
-  b=20
-  return 20
-  echo First Argument in Function = $1
-}
 
-xyz() {
-  echo I am a function xyz
 }
 
 ##  Main Program
@@ -16,7 +10,7 @@ a=10
 abc Rahul
 abc $1
 echo Exit status of abc - $?
-echo b in main program = $b
-xyz
+#echo b in main program = $b
+#xyz
 
 echo Frist Argument in Main Program = $1
