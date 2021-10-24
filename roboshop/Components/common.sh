16bcf941fcf63c87ab81624c58c179ac136ad6dd
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-24 23:41:09 +0500

    added git pull command

--- roboshop/Components/common.sh
+++ roboshop/Components/common.sh
@@ -4,15 +4,17 @@ Print() {
   echo -e "\n\e[36m======================== $1 ========================\e[0m" >>$LOG
 }
 
-Stat(){
-  if [$1 -eq 0]
-  then
-    echo  -e "\e[1;32m SUCCESS\e[0m"
-    else
-      echo  -e "\e[1;31m FAIL\e[0m"
-      echo  -e " \e[1;33m Script failed  Check for the details in $LOG file\e[0m "
-      exit 1
-      fi
+
+      Stat() {
+        if [ $1 -eq 0 ]
+         then
+          echo -e "\e[1;32mSUCCESS\e[0m"
+        else
+          echo -e "\e[1;31mFAILURE\e[0m"
+          echo -e "\e[1;33mScript Failed and check the detailed log in $LOG file\e[0m"
+          exit 1
+        fi
+      }
 }
 LOG=/tmp/roboshop.log
 rm -f  $LOG
