1d60898fd67835bdaf22f19dc995ef8187a4a116
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-25 19:36:28 +0500

    added git pull command

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -14,8 +14,10 @@ Stat  $?
  systemctl start nginx &>> $LOG
 Stat  $?
 
-exit
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+Print "Download Html Pages"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+Stat $?
+ #curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
  cd /usr/share/nginx/html
  rm -rf *
