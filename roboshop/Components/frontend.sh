b2016abc9e9c8d117a2280c9d3eb414070d36f3f
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-25 19:43:46 +0500

    added git pull command

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -1,31 +1,55 @@
 #!bin/bash
 
 source  Components/common.sh
+MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 Print  "Installing Nginx"
  yum install nginx -y &>> $LOG
  Stat $?
 
- Print  "Enabeling Nginx"
-  systemctl enable nginx &>> $LOG
-Stat  $?
+ #Print  "Enabeling Nginx"
+  #systemctl enable nginx &>> $LOG
+#Stat  $?
 
- Print  "Starting Nginx"
- systemctl start nginx &>> $LOG
-Stat  $?
+ #Print  "Starting Nginx"
+ #systemctl start nginx &>> $LOG
+#Stat  $?
 
 Print "Download Html Pages"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 Stat $?
- #curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
- cd /usr/share/nginx/html
- rm -rf *
- unzip /tmp/frontend.zip
- mv frontend-main/* .
- mv static/* .
- rm -rf frontend-master static README.md
- mv localhost.conf /etc/nginx/default.d/roboshop.conf
+Print "Remove Old Html Pages"
+rm -rf /usr/share/nginx/html/* &>>$LOG
+Stat $?
+
+Print "Extract Frontend Archive"
+unzip -o -d /tmp /tmp/frontend.zip &>>$LOG
+Stat $?
+
+Print "Copy files to Nginx path"
+mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$LOG
+Stat $?
+
+Print "Copy Nginx Roboshop Config file"
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf  &>>$LOG
+Stat $?
+
+Print "Enabling Nginx"
+systemctl enable nginx &>>$LOG
+Stat $?
+
+Print "Starting Nginx"
+systemctl restart nginx &>>$LOG
+Stat $?
+
+ #cd /usr/share/nginx/html
+ #rm -rf *
+ #unzip /tmp/frontend.zip
+ #mv frontend-main/* .
+ #mv static/* .
+ #rm -rf frontend-master static README.md
+ #mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 
- systemctl restart nginx
+ #systemctl restart nginx
