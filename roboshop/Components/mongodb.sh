b47a90b3765e3d9522766fab552a67eadc5cb73c
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-25 22:58:11 +0500

    created script for mongodb

--- roboshop/Components/mongodb.sh
+++ roboshop/Components/mongodb.sh
@@ -1,26 +1,41 @@
 #!bin/bash
 
 source  Components/common.sh
+MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
- curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-exit
-#Install Mongo & Start Service.
-# yum install -y mongodb-org
-# systemctl enable mongod
-# systemctl start mongod
-#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
-#Config file: /etc/mongod.conf
+Print  "Downloading  repo "
+ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
+Stat  $?
 
-#then restart the service
+Print "Installing Mongodb"
+ yum install -y mongodb-org &>>$LOG
+ Stat  $?
 
-# systemctl restart mongod
-#Every Database needs the schema to be loaded for the application to work.
-#Download the schema and load it.
+Print "Updating the mongod config"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG
+Stat  $?
 
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+Print "Enabeling mongodb"
+systemctl enable mongod &>>$LOG
+Stat $?
 
-# cd /tmp
-# unzip mongodb.zip
+Print "Start mongodb"
+ systemctl start mongod &>>$LOG
+ Stat $?
+
+Print   "Downloading schema for mongod"
+ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+ Stat $?
+
+ Print "Extract Schema"
+ unzip -o -d /tmp /tmp/mongodb.zip &>>$LOG
+ Stat $?
+
+Print "Load Schema"
+cd /tmp/mongodb-main
+mongo < catalogue.js &>>$LOG
+mongo < users.js &>>$LOG
+Stat $?
 # cd mongodb-main
 # mongo < catalogue.js
 # mongo < users.js
