23c1e5dc933ba1f1717859930327dd08895a95db
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-27 19:55:54 +0500

    updated script

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -2,8 +2,6 @@
 
  COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l )
 
-echo "count value" $COUNT
-
 if [  $COUNT -eq 0 ] ; then
     aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids  sg-017e24d5e5f10677e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
      else
