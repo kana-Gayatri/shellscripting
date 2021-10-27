0a5b3a20bd364374781be26aa903e6baed154dd7
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-27 19:15:25 +0500

    jscon script to get dnsname and ipaddress

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -5,8 +5,7 @@ CREATE()
 {
   COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | w -l)
 
-  if [ $COUNT -eq 0 ]
-   then
+  if [ $COUNT -eq 0 ] ; then
     aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids ids sg-017e24d5e5f10677e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
