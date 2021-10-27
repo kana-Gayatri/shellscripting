bf7df5f04ce85fe4ab12840b04eca8ef388b3928
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-27 23:14:30 +0500

    mongodb script

--- roboshop/Components/mongodb.sh
+++ roboshop/Components/mongodb.sh
@@ -13,15 +13,33 @@ Print "Start Mongo Service"
   sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG
   Stat $?
 
-Print  "Downloadi achive"
+Print  "Downloadi  MongoDB"
  curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"  &>>$LOG
+ # curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
   Stat $?
 
+  Print "Extract MongoDB Content"
+   # unzip mongodb.zip
+    unzip -o -d  /tmp/mongodb.zip &>>$LOG
+    Stat $?
+  # cd mongodb-main
+  #if [ "$1" == "/home/roboshop" ]; then
+    Print "Remove Old Content"
+    rm -rf /home/roboshop/mongodb
+    Stat $?
+
+    Print "Copy Content"
+    mv /home/roboshop/mongodb-main /home/roboshop/mongdb
+    Stat $?
+  #fi
+
+
+exit
 Print "Load Schema"
 cd  /tmp/mongodb-main  &>>$LOG
 Stat  $?
 
-exit
+
 for db in catalogue users ; do
   mongo < $db.js &>>$LOG
 done
