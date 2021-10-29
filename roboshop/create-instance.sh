770169fe29c79acc32ffb8bb6e243abce1a6cc03
Gayatri <gayatrikanaparthi@gmail.com>
2021-10-29 10:27:36 +0500

    added xargs as argument to remove quotes to IPAddress

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -11,5 +11,5 @@ if [  $COUNT -eq 0 ] ; then
  IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | xargs )
   ## xargs is used to remove the double  quotes
   sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z021234083EL1N0CXFXW --change-batch file:///tmp/record.json | jq  &>/dev/null
+  aws route53 change-resource-record-sets --hosted-zone-id Z021234083EL1N0CXFXW --change-batch file:///tmp/record.json | jq  &>/dev/null  | xargs
 
