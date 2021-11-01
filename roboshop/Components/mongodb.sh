cc08759af6259ed6b13f54d1a151324d647eb454
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-01 12:12:23 +0500

    modified mongodb script

--- roboshop/Components/mongodb.sh
+++ roboshop/Components/mongodb.sh
@@ -1,45 +1,36 @@
 #!/bin/bash
 
-source  Components/common.sh
+source components/common.sh
+MSPACE=$(cat $0 | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
-Print "Downloading Repo for mongo"
+COMPONENT_NAME=MongoDB
+COMPONENT=mongodb
+
+Print "Download Repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
 Stat $?
 
-Print "Installing  MongoDB "
- yum install -y mongodb-org &>>$LOG
-  Stat $?
-
-Print "Enabling the service"
-systemctl enable mongod &>>$LOG
+Print "Install MongoDB"
+yum install -y mongodb-org &>>$LOG
 Stat $?
+
+Print "Update MongoDB Config"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG
+Stat $?
+
+Print "Start MongoDB"
 systemctl restart mongod &>>$LOG
 Stat $?
 
-# systemctl restart mongod
+Print "Enable MongoDB Service"
+systemctl enable mongod &>>$LOG
+Stat $?
 
-  Print "Update MongoDB Config"
-  sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG
-  Stat $?
-
-Print  "Downloading  MongoDB"
- curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"  &>>$LOG
-  Stat $?
-
-  Print "Extract MongoDB Content"
-   unzip -o -d  /tmp   /tmp/mongodb.zip &>>$LOG
-    Stat $?
-
-  Print "Remove Old Content"
-      rm -rf /home/roboshop/mongodb &>>$LOG
-      Stat $?
+DOWNLOAD "/tmp"
 
 Print "Load Schema"
-cd  /tmp/mongodb-main  &>>$LOG
-Stat  $?
-   # mv  /home/roboshop/mongodb-main /home/roboshop/mongdb &>>$LOG
-    #mv /home/roboshop/${COMPONENT}-main /home/roboshop/${COMPONENT}
- mongo < catalogue.js &>>$LOG
- mongo < users.js&>>$LOG
-
-  Stat $?
+cd /tmp/mongodb-main
+for db in catalogue users ; do
+  mongo < $db.js &>>$LOG
+done
+Stat $?
