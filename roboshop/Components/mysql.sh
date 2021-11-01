a7303383b0e5f89c9d61121e25d5eed3a2156d46
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-01 12:13:41 +0500

    modified mysql script

---
+++ roboshop/Components/mysql.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source components/common.sh
+MSPACE=$(cat $0 | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+
+COMPONENT_NAME=MongoDB
+COMPONENT=mongodb
+
+Print "Download Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
+Stat $?
+
+Print "Install MongoDB"
+yum install -y mongodb-org &>>$LOG
+Stat $?
+
+Print "Update MongoDB Config"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG
+Stat $?
+
+Print "Start MongoDB"
+systemctl restart mongod &>>$LOG
+Stat $?
+
+Print "Enable MongoDB Service"
+systemctl enable mongod &>>$LOG
+Stat $?
+
+DOWNLOAD "/tmp"
+
+Print "Load Schema"
+cd /tmp/mongodb-main
+for db in catalogue users ; do
+  mongo < $db.js &>>$LOG
+done
+Stat $?
