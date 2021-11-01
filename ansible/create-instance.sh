18d77466ede69a50b80504c7f693f67bfb1ba380
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-01 12:52:37 +0500

    image id modified

--- ansible/create-instance.sh
+++ ansible/create-instance.sh
@@ -2,7 +2,7 @@
  COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l )
 
 if [  $COUNT -eq 0 ] ; then
-    aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids  sg-017e24d5e5f10677e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+    aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --security-group-ids  sg-017e24d5e5f10677e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
      else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
   fi
