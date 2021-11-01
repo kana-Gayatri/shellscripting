aa80f6c59dd65db7ac3417050e799b289382675c
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-01 12:09:46 +0500

    catalogue script

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -66,3 +66,16 @@ Print "Start System service"
  systemctl daemon-reload
  systemctl start catalogue
  systemctl enable catalogue
+
+
+#!/bin/bash
+
+source components/common.sh
+
+MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+
+
+COMPONENT_NAME=Catalogue
+COMPONENT=catalogue
+NODEJS
+CHECK_MONGO_FROM_APP
