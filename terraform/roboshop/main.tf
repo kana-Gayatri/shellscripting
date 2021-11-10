a3a7f983fa7bdb7f2274b4f122cff665e763361f
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-10 19:36:42 +0500

    roboshop main script

--- terraform/roboshop/main.tf
+++ terraform/roboshop/main.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
      count =  length(var.components)
     ami           =  data.aws_ami.ami.id
     instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg.."]
+  vpc_security_group_ids = ["sg-017e24d5e5f10677e"]
 
   tags = {
     Name = element(var.components, count.index )
