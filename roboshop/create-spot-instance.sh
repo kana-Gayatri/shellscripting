922f9ac14003c21b3e2ae2cfd18b684fd908f3ab
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-12 08:56:57 +0500

    modified version to 4

--- roboshop/create-spot-instance.sh
+++ roboshop/create-spot-instance.sh
@@ -6,6 +6,7 @@ CREATE() {
   if [ $COUNT -eq 0 ]; then
     aws ec2 run-instances --launch-template LaunchTemplateId=lt-0d2a53a2437f3f637,Version=4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
 
+
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
     return
